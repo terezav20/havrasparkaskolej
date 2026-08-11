5a89f372b6f7315af3753c4f2ec33aae81c135f5
terezav20 <tereza.vocilkova01@upol.cz>
2026-08-11 21:47:32 +0200

    Update image logic in kolej.js and add evening image for better time-based display

--- kolej.js
+++ kolej.js
@@ -666,7 +666,8 @@
     if (hour >= 6 && hour < 8) image = "kolejky_ruzne/kolejka_vychodslunce.png";
     else if (hour >= 19 && hour < 20) image = "kolejky_ruzne/kolejka_svitani.png";
     else if (hour >= 20 && hour < 21) image = "kolejky_ruzne/kolejka_zapadslunce.png";
-    else if (hour >= 21 || hour < 6) image = "kolejky_ruzne/kolejka_noc.png";
+    else if (hour >= 21 || hour < 0) image = "kolejky_ruzne/kolejka_noc.png";
+    else if (hour >= 0 || hour < 6) image = "kolejky_ruzne/kolejka_vecer.png";
 
     if (img.getAttribute('src') !== image) {
         img.src = image;
