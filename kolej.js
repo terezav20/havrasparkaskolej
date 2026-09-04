de21eb663100118fc8dde1dd36b2154467b0bcc5
terezav20 <tereza.vocilkova01@upol.cz>
2026-09-04 13:12:07 +0200

    Refactor code structure for improved readability and maintainability

--- kolej.js
+++ kolej.js
@@ -688,7 +688,7 @@
         if (!img) return;
 
         const hour = new Date().getHours();
-        let image = "kolejky_ruzne/kolej/kolejka.webp";
+        let image = "kolejky_ruzne/kolej/narozeniny_apo.webp";
 
         if (hour >= 6 && hour < 8) image = "kolejky_ruzne/kolej/kolejka_vychodslunce.webp";
         else if (hour >= 19 && hour < 20) image = "kolejky_ruzne/kolej/kolejka_zapadslunce.webp";
